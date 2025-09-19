0bab6a46c019f4451360b324531b7ce9026547d5
MichalMroz06 <michalmroz2006@gmail.com>
2025-09-19 10:41:17 +0200

    Dodanie podstawowych plików projektu

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace app;
+
+public class Euclid
+{
+    public static int Nwd(int a, int b)
+    {
+        if (a == 0 && b == 0)
+            throw new ArgumentException("NWD nie jest zdefiniowane dla (0, 0).");
+
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    static void Main(string[] args)
+    {
+        
+    }
+}
